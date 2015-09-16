75b2941ab47a442dee5a871c49571fa428c9d025
jagtej <jagtej@yahoo-inc.com>
2015-09-16 11:43:17 -0700

    Initial Commit

---
+++ ChatClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatClient
+//
+//  Created by Jagtej Sodhi on 9/16/15.
+//  Copyright © 2015 Jagtej Sodhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatClient
+//
+//  Created by Jagtej Sodhi on 9/16/15.
+//  Copyright © 2015 Jagtej Sodhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
